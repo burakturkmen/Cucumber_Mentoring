69f2359748b8834eb86515586f80e25675445127
burakturkmen <Brktrkmn96>
2020-11-01 13:56:16 +0100

    xz

---
+++ src/test/java/Cucumber_Proje.java
@@ -0,0 +1,45 @@
+public class Cucumber_Proje {
+
+    /*
+
+    1) Takımınız ile bir framework oluşturun.
+        1.1) Birlikte Dependencies oluşturun.
+        1.2) Framework'ün temel kısımlarını da birlikte oluşturun. (Driver Class, Parent Class)
+
+    2) Birlikte oluşturulan 1 framework push edilmeli ve geri kalanlar, projeyi clone etmeli.
+            Clone edildikten sonra herkes kendi görevi olan kısmı yapmalı.
+
+    3) Üstünde çalışmanız gereken Feature'lar bunlar;
+
+              3.1) Create a Country, Delete a Country
+              3.2) Create a City, Delete a City
+              3.3) Create a Country (SOMECOUNTRY) and Create a City (Açılan dropdown'dan seçiniz.)
+
+              3.4) Remove Country and City. (Önce country'i silmeyi deneyin, bir hata mesajı göremlisiniz.)
+                       ***  Country için bir city oluşturduktan sonra silinmemelidir. Bu adımı da test etmelisiniz. ***
+
+              3.5) Create a Education> Setup> SubjectCategories delete a SubjectCategories
+              3.6) Create a Education > Setup > Subjects delete a Subjects
+
+              3.7) Create a Education > Setup > SubjectCategories(name is MySubjectCat ) and create a Education> Setup> Subjects
+                                (Subject Category olarak MySubjectCat seçiniz.)
+                                        *** Subject oluşturduktan sonra, Subject Categories'i silmeyi deneyin. Silinmediğinden emin olun. Bu adımı da test edin. ***
+
+              3.8) Create a Human Resources > Setup > Position Salary , edit the position Salary , delete the position Salary
+                        Create a Human Resources > Setup > Positions , edit the Positions , delete the Positions
+
+              3.9) Create a Human Resources > Setup > Position Salary , edit the position Salary , delete the position Salary
+                        Create a Human Resources > Setup > Positions , edit the Positions , delete the Positions
+
+              3.10) Create a Human Resources > Setup > Salary Modifiers , edit the Salary Modifiers , delete the Salary Modifiers
+                        Create a Human Resources > Setup > Salary Constants , edit the Salary Constants , delete the Salary Constants
+
+              3.11) Create a Budget > Setup > Budget Groups , edit the Budget Groups , delete the Budget Groups
+              3.12) Create a Budget > Setup > Cost Centers , edit the Cost Centers  , delete the Cost Centers
+
+     4) Herkes Features'ı bitirdiğinde Github'a push yapın. Eğer bir sorun varsa sorunu çözün.
+
+
+
+     */
+}
